e79226701f190381dd8247ebcb780111bd852352
karthik-sethuraman <karthik.sethuraman@necam.com>
2018-01-27 02:10:56 -0500

    Removing old TAPI 1.0 RI files

--- TAPI_RI/app.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-import connexion
-
-if __name__ == '__main__':
-    app = connexion.App(__name__, specification_dir='./swagger/')
-    app.add_api('swagger.yaml', arguments={'title': 'tapi-connectivity API generated from tapi-connectivity.yang'})
-    app.run(port=8080)
